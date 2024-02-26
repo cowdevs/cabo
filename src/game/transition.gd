8da2d78d52ff2cf115fdacef28a6929c821f11d6
Luke L <chocopikachu752@gmail.com>
2024-02-26 01:00:45 -0800

    Fixed transition + card sfx
    Updated Feb 26, 2024

--- src/game/transition.gd
+++ src/game/transition.gd
@@ -2,37 +2,21 @@ extends CanvasLayer
 
 const DELAY := 0.05
 
-signal transitioned
-
 func change_scene(path: String) -> void:
 	show()
-	fade_out()
-	await transitioned
+	for column in $Row.get_children():
+		column.get_node('AnimationPlayer').play_backwards("fade")
+		column.get_node('AnimationPlayer').speed_scale = 0
+	for column in $Row.get_children():
+		column.get_node('AnimationPlayer').speed_scale = 1.0
+		await get_tree().create_timer(DELAY).timeout
+	await $Row.get_child(5).get_node('AnimationPlayer').animation_finished
 	get_tree().change_scene_to_file(path)
-	fade_in()
-	await transitioned
+	for column in $Row.get_children():
+		column.get_node('AnimationPlayer').play("fade")
+		column.get_node('AnimationPlayer').speed_scale = 0
+	for column in $Row.get_children():
+		column.get_node('AnimationPlayer').speed_scale = 1.0
+		await get_tree().create_timer(DELAY).timeout
+	await $Row.get_child(5).get_node('AnimationPlayer').animation_finished
 	hide()
-
-func fade_in() -> void:
-	for column in $Row.get_children():
-		column.get_node('AnimationPlayer').play("fade_in")
-		column.get_node('AnimationPlayer').speed_scale = 0.1
-	
-	for column in $Row.get_children():
-		column.get_node('AnimationPlayer').speed_scale = 1.0
-		await get_tree().create_timer(DELAY).timeout
-	
-	await $Row.get_child(5).get_node('AnimationPlayer').animation_finished
-	emit_signal("transitioned")
-
-func fade_out() -> void:
-	for column in $Row.get_children():
-		column.get_node('AnimationPlayer').play("fade_out")
-		column.get_node('AnimationPlayer').speed_scale = 0.1
-	
-	for column in $Row.get_children():
-		column.get_node('AnimationPlayer').speed_scale = 1.0
-		await get_tree().create_timer(DELAY).timeout
-	
-	await $Row.get_child(5).get_node('AnimationPlayer').animation_finished
-	emit_signal("transitioned")
